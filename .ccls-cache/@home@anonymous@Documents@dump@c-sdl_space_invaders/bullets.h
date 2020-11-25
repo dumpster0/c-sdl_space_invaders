b0f29fb7e202bec83ba29e4721b6b6fbd1c1517c
panic-panic <stackoverflowpanic@gmail.com>
2020-11-25 09:55:00 +0530

    debugged skeleton

--- .ccls-cache/@home@anonymous@Documents@dump@c-sdl_space_invaders/bullets.h
+++
@@ -1,9 +0,0 @@
-#ifndef BULLETS_H
-#define BULLETS_H
-
-typedef struct {
-  position pos;
-  character shooter; 
-} bullet;
-
-#endif
--- .ccls-cache/@home@anonymous@Documents@dump@c-sdl_space_invaders/player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-typedef struct {
-  position pos;
-  int lives;
-} player;
-
-#endif
